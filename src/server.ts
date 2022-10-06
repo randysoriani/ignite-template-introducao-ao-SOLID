86fcc27e7fd1cbad20f8d5ac84a45089f2c011e6
Randy Soriani <randysoriani@outlook.com>
2022-10-05 22:37:43 -0300

    wip: add swagger

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,8 @@
 import { app } from ".";
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 
 app.listen(3333, () => console.log("Server is running!"));
